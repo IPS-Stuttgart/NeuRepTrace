bcb94c14bb7dbda70d9070844b664a8daa0abbb7
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-21 18:35:06 +0200

    Synchronize package __version__ with pyproject

--- src/neureptrace/__init__.py
+++ src/neureptrace/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 __all__ = ["__version__"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from neureptrace import _decoding_regularization_patch, _event_detection_extensions
 
