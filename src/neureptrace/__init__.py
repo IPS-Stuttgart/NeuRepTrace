d653d41142aded7abc823018fe8d0795d15af65b
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-23 22:08:39 +0200

    Install finite C-grid validation patch

--- src/neureptrace/__init__.py
+++ src/neureptrace/__init__.py
@@ -6,6 +6,7 @@ __all__ = ["__version__"]
 __version__ = "0.1.1"
 
 from neureptrace import (  # noqa: E402
+    _decoding_c_grid_patch,
     _decoding_probability_patch,
     _decoding_regularization_patch,
     _event_detection_extensions,
@@ -13,4 +14,5 @@ from neureptrace import (  # noqa: E402
 
 _event_detection_extensions.install()
 _decoding_regularization_patch.install()
+_decoding_c_grid_patch.install()
 _decoding_probability_patch.install()
