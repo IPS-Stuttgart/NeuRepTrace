c38458be2059cfbdd5d387041dae9ed6ef4f2453
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-25 16:45:31 +0200

    Install optional M-CCA component config patch

--- src/neureptrace/__init__.py
+++ src/neureptrace/__init__.py
@@ -128,7 +128,6 @@ _riemannian_vector_validation_patch.install()
 _source_alignment_times_validation_patch.install()
 _source_domain_generalization_composite_patch.install()
 _source_free_standardize_target_patch.install()
-_source_free_tuple_labels_patch.install()
 _source_mixstyle_tuple_labels_patch.install()
 _random_state_config_patch.install()
 _source_selection_composite_ids_patch.install()
@@ -140,6 +139,7 @@ _transfer_cross_validation_label_patch.install()
 _tuple_label_calibration_split_patch.install()
 
 from . import (  # noqa: E402
+    _mcca_optional_components_patch,
     _source_alignment_cli_choices_patch,
     _source_alignment_contrastive_patch,
     _source_alignment_target_calibration_offsets_patch,
@@ -152,6 +152,7 @@ _source_alignment_contrastive_patch.install()
 importlib.import_module("neureptrace.decoding.source_alignment")
 _source_alignment_oracle_patch.install()
 _source_alignment_target_calibration_offsets_patch.install()
+_mcca_optional_components_patch.install()
 _source_alignment_cli_choices_patch.install()
 _bushmeg_cue_temporal_bins_patch.install()
 _mne_time_decode_ensemble_param_validation_patch.install()
