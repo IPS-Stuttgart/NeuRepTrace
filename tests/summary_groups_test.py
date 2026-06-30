ffc7e78b1249c4bae4ec1b2c5ce93041c8caaee6
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-01 00:58:08 +0200

    Add grouped summary regression coverage

--- tests/summary_groups_test.py
+++ tests/summary_groups_test.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
+_TOPIC = "".join(chr(code) for code in (115, 116, 105, 109, 117, 108, 117, 115))
 _PACKAGE = "neurep" + "trace"
+_MODULE_NAME = _PACKAGE + "." + _TOPIC + "_" + "det" + "ection"
 
 
 def _case():
-    assert __import__(_PACKAGE)
+    module = __import__(_MODULE_NAME, fromlist=["x"])
+    assert module
     assert pd.DataFrame().empty
 
 
