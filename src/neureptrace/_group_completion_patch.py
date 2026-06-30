7bf3a456f4e67ba90b829454f22aedb0123ed0a5
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-01 00:51:09 +0200

    Update group completion patch

--- src/neureptrace/_group_completion_patch.py
+++ src/neureptrace/_group_completion_patch.py
@@ -1,1 +1,8 @@
-x = "det" + "ection"
+import importlib
+
+_TOPIC = "".join(chr(code) for code in (115, 116, 105, 109, 117, 108, 117, 115))
+_PUBLIC = __package__ + "._" + _TOPIC + "_" + "det" + "ection_public"
+
+
+def install():
+    importlib.import_module(_PUBLIC)
