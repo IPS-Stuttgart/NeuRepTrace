b4738d2d88b662599574d4370466006a11e61f1f
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-04 09:34:06 +0200

    Install oscillatory single window patch

--- src/neureptrace/features/__init__.py
+++ src/neureptrace/features/__init__.py
@@ -2,10 +2,11 @@
 
 from __future__ import annotations
 
-from . import _oscillatory_composite_labels_patch
+from . import _oscillatory_composite_labels_patch, _oscillatory_single_window_patch
 from . import oscillatory as _oscillatory
 
 _oscillatory_composite_labels_patch.install()
+_oscillatory_single_window_patch.install()
 
 BandFeatureWindow = _oscillatory.BandFeatureWindow
 compute_alpha_features = _oscillatory.compute_alpha_features
